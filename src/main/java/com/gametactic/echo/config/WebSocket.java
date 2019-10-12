009b5780d8aab1120e4ce2c4dd664a7cc480ec5b
Niko Granö <niko@ironlions.fi>
2019-10-12 21:09:40 +0300

    Fix CORS.

--- src/main/java/com/gametactic/echo/config/WebSocket.java
+++ src/main/java/com/gametactic/echo/config/WebSocket.java
@@ -21,7 +21,10 @@ public class WebSocket implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry
+                .addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Override
